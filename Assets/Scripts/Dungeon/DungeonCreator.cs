71d30e51230dd4729189ca033ca02abccf624e92
Morphey <n.zelencov@bit.games>
2023-09-02 17:33:23 +0300

    random first dungeon room

--- Assets/Scripts/Dungeon/DungeonCreator.cs
+++ Assets/Scripts/Dungeon/DungeonCreator.cs
@@ -9,26 +9,27 @@ public class DungeonCreator
 
   private readonly AssetsProviderService _assetsProvider;
   private readonly RandomnessService _randomnessService;
-  private readonly Configs _config;
+  private readonly Configs _configs;
 
   [Inject]
   public DungeonCreator(AssetsProviderService assetsProvider, RandomnessService randomService, Configs configs)
   {
     _assetsProvider = assetsProvider;
     _randomnessService = randomService;
-    _config = configs;
+    _configs = configs;
 
     _roomPrefabsCache = new(10);
   }
 
   public async Task Create(string id)
   {
-    var config = _config.GetConf<ConfDungeon>(id);
+    var config = _configs.GetConf<ConfDungeon>(id);
     await LoadRoomsBy(config);
     
     var dungeonGo = new GameObject($"Dungeon: {config.ID}");
-    var roomsCount = _randomnessService.RandomInt(0, _roomPrefabsCache.Count);
-    var firstRoom = _assetsProvider.Create(_roomPrefabsCache[0], dungeonGo.transform);
+    var roomsCount = _randomnessService.RandomInt(0, config.MaxRoomsCount);
+    var firstRoomIndex = _randomnessService.RandomInt(0, _roomPrefabsCache.Count - 1);
+    var firstRoom = _assetsProvider.Create(_roomPrefabsCache[firstRoomIndex], dungeonGo.transform);
     firstRoom.transform.localPosition = Vector3.zero;
   }
 
